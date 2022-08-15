4b58d1ca251fc7c0d2016759b1adfaafd0d0f9eb
Santadd <hethere97@gmail.com>
2022-08-15 16:05:21 +0000

    Heroku Celery Setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -146,7 +146,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
 # Celery Settings
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL') or 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
